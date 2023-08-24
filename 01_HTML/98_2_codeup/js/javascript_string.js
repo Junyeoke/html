da3758373aaabcff4e1f7649d0a479367f7bf9da
Junyeoke <junyeoke@kakao.com>
2023-08-24 17:44:35 +0900

    24일

---
+++ 01_HTML/98_2_codeup/js/javascript_string.js
@@ -0,0 +1,26 @@
+// TODO: 1131
+// let input = prompt("문자하나입력");
+// console.log(input);
+
+// TODO: 1132
+// let input = prompt("단어입력");
+// console.log(input);
+
+// TODO: 1133 
+// let input = prompt("공백이 있는 문자열 입력");
+// console.log(input);
+
+
+// TODO: 1295
+
+// TODO: 1406
+// TODO: 1408
+// TODO: 1410
+// TODO: 1414 
+// TODO: 1418 
+// TODO: 1419 
+// TODO: 1733 
+// TODO: 1734
+// TODO: 1754
+// TODO: 1990
+// TODO: 2721
